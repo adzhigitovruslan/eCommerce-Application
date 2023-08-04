2dc08ac298ddb595239fe6494daab30f01c2c92e
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-08-04 14:27:46 +0400

    feat: add Husky pre-commit hook for formatting, linting, and tests

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,28 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
-</template>
+  <template />
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  <style lang="scss">
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
     }
-  }
-}
-</style>
+
+    nav {
+      padding: 30px;
+
+      a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+          color: #42b983;
+        }
+      }
+    }
+  </style>
+</template>
