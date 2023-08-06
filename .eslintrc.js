8fc675a47174e7faaf40339cc26761df9b0496b8
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-08-06 19:07:10 +0400

    feat: add new configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,24 +4,45 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: [2, 'always'],
+    'max-len': ['error', { code: 120, ignorePattern: '^import\\s.+\\sfrom\\s.+;$' }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
+    quotes: ['error', 'single', { allowTemplateLiterals: true, avoidEscape: true }],
+    'import/extensions': 'off',
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'max-lines-per-function': ['error', 40],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['if', 'for', 'while', 'switch'],
+      },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let'], next: '*' },
+      { blankLine: 'always', prev: '*', next: ['const', 'let'] },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let'],
+        next: ['export', 'const', 'let'],
+      },
+    ],
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
