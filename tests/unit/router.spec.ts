10f528ba17abde5cd7eb8ae0f088b25d771de2ff
taorminak <ostanina.anna@yahoo.com>
2023-08-21 02:13:59 +0200

    feat: add test router

---
+++ tests/unit/router.spec.ts
@@ -0,0 +1,77 @@
+import { mount, VueWrapper } from '@vue/test-utils';
+import { Router, createRouter, createWebHistory } from 'vue-router';
+import App from '@/App.vue';
+import HomeView from '@/views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+import LoginView from '@/views/LoginView.vue';
+import CartView from '@/views/CartView.vue';
+import CatalogView from '@/views/CatalogView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import { Component } from 'vue';
+
+const setupRouter = () => {
+  return createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/', component: HomeView },
+      { path: '/about', component: AboutView },
+      { path: '/login', component: LoginView },
+      { path: '/cart', component: CartView },
+      { path: '/catalog', component: CatalogView },
+      { path: '/:catchAll(.*)', component: NotFoundView },
+    ],
+  });
+};
+
+const setupWrapper = (router: Router) => {
+  return mount(App, {
+    global: {
+      plugins: [router],
+    },
+  });
+};
+
+const navigateAndAssertViewExists = async (
+  router: Router,
+  wrapper: VueWrapper,
+  path: string,
+  viewComponent: Component,
+) => {
+  await router.push(path);
+  await router.isReady();
+  expect(wrapper.findComponent(viewComponent).exists()).toBe(true);
+};
+
+describe('App', () => {
+  let router: Router;
+  let wrapper: VueWrapper;
+
+  beforeEach(() => {
+    router = setupRouter();
+    wrapper = setupWrapper(router);
+  });
+
+  it('renders the home view when the route is /', async () => {
+    await navigateAndAssertViewExists(router, wrapper, '/', HomeView);
+  });
+
+  it('renders the about view when the route is /about', async () => {
+    await navigateAndAssertViewExists(router, wrapper, '/about', AboutView);
+  });
+
+  it('renders the login view when the route is /login', async () => {
+    await navigateAndAssertViewExists(router, wrapper, '/login', LoginView);
+  });
+
+  it('renders the cart view when the route is /cart', async () => {
+    await navigateAndAssertViewExists(router, wrapper, '/cart', CartView);
+  });
+
+  it('renders the catalog view when the route is /catalog', async () => {
+    await navigateAndAssertViewExists(router, wrapper, '/catalog', CatalogView);
+  });
+
+  it('renders the not-found view when the route is unknown', async () => {
+    await navigateAndAssertViewExists(router, wrapper, '/unknown', NotFoundView);
+  });
+});
