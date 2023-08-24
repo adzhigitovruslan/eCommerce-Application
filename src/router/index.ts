83a6a5800766713a535014296f5c66ee04c9e63f
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-08-24 19:44:52 +0400

    fix: fix configs

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import store from '@/store/index.ts';
+import store from '@/store/index';
 
 const routes: Array<RouteRecordRaw> = [
   {
