9bd861d57d4a75160f723d8731d6a1bfeb67503d
taorminak <ostanina.anna@yahoo.com>
2023-09-15 11:10:32 +0200

    feat: add adaptive design to about us page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container-about">
     <header class="about__header">
-      <div>
+      <div class="header__logo">
         <h1>Development Team of <br />Online Store</h1>
-        <div class="about__store_logo">
+        <div class="about__store_icon">
           <br /><img src="/icon_logo2.png" alt="logo" class="about__logo" />
           <span>Playnchill</span>
         </div>
       </div>
-
-      <img src="@/assets/images/Illustration.svg" alt="Illustration" class="header__img" />
+      <div class="header__img-container">
+        <img src="@/assets/images/Illustration.svg" alt="Illustration" class="header__img" />
+      </div>
     </header>
 
     <section class="about__team">
@@ -47,14 +48,22 @@
         quality and innovative solutions.
       </p>
     </section>
+
+    <section class="about__school">
+      <ul>
+        <li>
+          <img src="https://rs.school/images/rs_school_js.svg" alt="RS School" />
+        </li>
+        <li><a href="https://rs.school/js/">The Rolling Scopes School</a></li>
+        <li>2023</li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import teamMembersData from '@/teamMembers.json';
 import { defineComponent } from 'vue';
-
-console.log(teamMembersData);
 
 export default defineComponent({
   data() {
@@ -64,8 +73,6 @@ export default defineComponent({
   },
   methods: {
     getImagePath(photo: string): string {
-      console.log(photo, `@/assets/images/${photo}`);
-
       return require(`@/assets/images/${photo}`);
     },
   },
@@ -76,7 +83,7 @@ export default defineComponent({
 .container-about {
   width: 80vw;
   margin: auto;
-  font-size: 44px;
+  font-size: 2.75rem;
   font-weight: 800;
   line-height: 83px;
   letter-spacing: 0em;
@@ -92,15 +99,19 @@ export default defineComponent({
     text-align: center;
     margin-top: 100px;
 
+    .header__logo {
+      padding: 50px;
+    }
+
     h1 {
-      font-size: 42px;
+      font-size: 1em;
       font-weight: 800;
       line-height: 83px;
       letter-spacing: 0em;
       text-align: center;
     }
 
-    .about__store_logo {
+    .about__store_icon {
       display: flex;
       align-items: center;
       justify-content: center;
@@ -109,10 +120,10 @@ export default defineComponent({
         height: 59px;
       }
     }
-
     .header__img {
-      max-width: 600px;
-      height: auto;
+      max-width: 100%;
+      max-height: 100%;
+      padding: 10px;
     }
   }
 
@@ -179,6 +190,7 @@ export default defineComponent({
         text-align: justify;
         margin: 0;
         color: rgba(255, 255, 255, 0.7);
+        padding: 10px;
       }
 
       a {
@@ -187,6 +199,7 @@ export default defineComponent({
         display: block;
         margin-top: 10px;
         font-size: 22px;
+        padding: 10px;
       }
     }
   }
@@ -213,6 +226,107 @@ export default defineComponent({
       margin-top: 50px;
       color: rgba(255, 255, 255, 0.7);
       text-indent: 35px;
+    }
+  }
+
+  .about__school {
+    margin-top: 50px;
+    margin-bottom: 50px;
+    ul {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      list-style: none;
+      margin: auto;
+      padding-inline-start: 0;
+      justify-content: flex-end;
+      margin-bottom: 5px;
+
+      li {
+        margin-left: 20px;
+        font-size: 22px;
+        color: #77be1d;
+
+        img {
+          background-color: white;
+          width: 100px;
+          height: auto;
+          display: block;
+          margin: 0 auto;
+
+          &:hover {
+            transform: scale(1.1);
+            transition: transform 0.3s ease;
+          }
+        }
+
+        a {
+          text-decoration: none;
+          color: rgba(255, 255, 255, 0.7);
+          font-weight: bold;
+          font-size: 22px;
+
+          &:hover {
+            color: #77be1d;
+            text-decoration: none;
+            transition: background-color 0.3s ease;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+}
+@media (max-width: 769px) {
+  .container-about {
+    font-size: 32px;
+
+    .about__team::before {
+      background: none;
+    }
+    .about__header {
+      flex-direction: column-reverse;
+
+      .header__img {
+        margin-bottom: 30px;
+      }
+
+      .about__store_icon {
+        .about__logo {
+          height: 38px;
+        }
+        span {
+          font-size: 32px;
+        }
+      }
+
+      h1 {
+        font-size: 28px;
+        line-height: 43px;
+      }
+    }
+    .about__collaboration h2 {
+      font-size: 28px;
+      line-height: 40px;
+    }
+    .about__team {
+      display: grid;
+      grid-template-columns: 1fr;
+      gap: 20px;
+      .container__team__member .team-member-card h2 {
+        font-size: 26px;
+        font-weight: 400;
+        line-height: 39px;
+      }
+    }
+
+    .about__school ul {
+      flex-direction: column;
+      justify-content: center;
+
+      li a {
+        font-size: 18px;
+      }
     }
   }
 }
